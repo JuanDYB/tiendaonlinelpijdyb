dd4159bfe78906e0fe3b9cec69e327ebd821257b
jdyb04jdyb@gmail.com <jdyb04jdyb@gmail.com@f79637ae-8f6c-4540-eb6d-f7dc2334ca86>
2011-10-31 11:12:57 +0000

    Implementado bloqueo de inicio de sesión después de intentos fallidos. Problema pendiente con el guardado de log.

--- OnlineShop/src/java/control/AuthServlet.java
+++ OnlineShop/src/java/control/AuthServlet.java
@@ -4,10 +4,13 @@ import modelo.Carrito;
 import modelo.Usuario;
 import java.io.IOException;
 import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import org.owasp.esapi.errors.IntrusionException;
 import org.owasp.esapi.errors.ValidationException;
 import persistencia.PersistenceInterface;
@@ -17,48 +20,58 @@ import persistencia.PersistenceInterface;
  */
 public class AuthServlet extends HttpServlet {
 
-    
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         if (validateForm(request) == false) {
             request.setAttribute("resultados", "Error iniciando sesion");
             Tools.anadirMensaje(request, "El formulario enviado no es correcto");
             request.getRequestDispatcher("/login.jsp").forward(request, response);
+        } else if (request.getSession().getAttribute("intentosLogin") != null && (Integer)request.getSession().getAttribute("intentosLogin") >= 5) {
+            request.setAttribute("resultados", "Innicio de sesión bloqueado");
+            Tools.anadirMensaje(request, "Se han superado el número de intentos de inicio de sesión, se ha bloqueado el incio de sesión");
+            Tools.anadirMensaje(request, "Deberá esperar unos minutos para volver a intentarlo");
+            request.getRequestDispatcher("/login.jsp").forward(request, response);
+            this.starTimer(request.getSession());
         } else {
-
             try {
                 String email = Tools.validateEmail(request.getParameter("email"));
                 String password = Tools.validatePass(request.getParameter("pass"));
 
-                PersistenceInterface persistence = (PersistenceInterface)
-                        request.getServletContext().getAttribute("persistence");
+                PersistenceInterface persistence = (PersistenceInterface) request.getServletContext().getAttribute("persistence");
 
                 Usuario user = persistence.getUser(email);
                 if (user != null) {
-                    if (Tools.generateMD5Signature(password +
-                            password.toLowerCase()).equals(user.getPass()) == true) {
+                    if (Tools.generateMD5Signature(password
+                            + password.toLowerCase()).equals(user.getPass()) == true) {
                         request.getSession().setAttribute("auth", true);
                         request.getSession().setAttribute("usuario", user.getMail());
 
                         Carrito carro = persistence.requestLastIncompleteCart(user.getMail());
-                        if (carro != null){
+                        if (carro != null) {
                             request.getSession().setAttribute("carrito", carro);
                         }
-                        if (request.getSession().getAttribute("requestedPage") != null){
+                        if (request.getSession().getAttribute("requestedPage") != null) {
                             String redirect = (String) request.getSession().getAttribute("requestedPage");
                             request.getSession().removeAttribute("requestedPage");
                             response.sendRedirect(redirect);
-                        }else{
+                        } else {
                             response.sendRedirect("/index.jsp");
                         }
+                        request.getSession().removeAttribute("intentosLogin");
                         return;
                     } else {
                         Tools.anadirMensaje(request, "La contraseña introducida es incorrecta");
-                        Tools.anadirMensaje(request, "Haga click <a href=\"/recoverpass?email=" +
-                                user.getMail() + "\" >aquí</a> si olvidó la contraseña y desea recuperarla");
+                        Tools.anadirMensaje(request, "Haga click <a href=\"/recoverpass?email="
+                                + user.getMail() + "\" >aquí</a> si olvidó la contraseña y desea recuperarla");
                     }
                 } else {
                     Tools.anadirMensaje(request, "No se ha encontrado ningún usuario con los datos especificados");
+                }
+
+                if (request.getSession().getAttribute("intentosLogin") == null) {
+                    request.getSession().setAttribute("intentosLogin", 1);
+                } else {
+                    request.getSession().setAttribute("intentosLogin", (Integer) request.getSession().getAttribute("intentosLogin") + 1);
                 }
                 request.setAttribute("resultados", "Error inciando sesion");
                 request.getRequestDispatcher("/login.jsp").forward(request, response);
@@ -76,27 +89,40 @@ public class AuthServlet extends HttpServlet {
 
     protected boolean validateForm(HttpServletRequest request) {
         Map<String, String[]> param = request.getParameterMap();
-        if (param.size() == 3 && param.containsKey("email") && param.containsKey("pass") &&
-                param.containsKey("login")) {
+        if (param.size() == 3 && param.containsKey("email") && param.containsKey("pass")
+                && param.containsKey("login")) {
             return true;
         } else {
             Tools.anadirMensaje(request, "El formulario enviado no tiene el formato correcto");
             return false;
         }
     }
-    
+
+    protected void starTimer(final HttpSession sesion) {
+        TimerTask timerTask = new TimerTask() {
+
+            @Override
+            public void run() {
+                sesion.invalidate();
+            }
+        };
+        
+        Timer timer = new Timer ();
+        timer.schedule(timerTask, 600000);
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.sendError(404);
     }
-    
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
-    
+
     @Override
     public String getServletInfo() {
         return "Servlet para la autentificación de usuarios";
